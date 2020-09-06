5f0340d2a4a8ec61804884e9db2c7a4cc1a33bcf
kingspp <kingspprathyush@gmail.com>
2020-09-06 14:46:40 -0400

    Adding base constructs -> Schema Class

---
+++ src/epyodbc/constructs/schema.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+| **@created on:** 9/4/20,
+| **@author:** prathyushsp,
+| **@version:** v0.0.1
+|
+| **Description:**
+| 
+|
+| **Sphinx Documentation Status:** 
+"""
+from epyodbc.constructs.base_class import BaseClass
+from epyodbc.constructs.table import Table
+
+
+class Schema(BaseClass):
+    def __init__(self, database: str):
+        self.database = database
+        self.tables = []
+
+    def add_table(self, table: Table):
+        self.tables.append(table)
+
+    def pretty(self):
+        return {
+            "database": self.database,
+            "tables": [t.pretty() for t in self.tables]
+        }
